0de44217318d01306b668b95adb819098df00a5a
Tim Macdonald <macdonald.tim@gmail.com>
2017-12-06 22:38:51 -0500

    Added isEndState and score calculators

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,6 +1,13 @@
 import actions from './actions'
 import inningReducer, { defaultInning } from './inningReducer'
 
+/**
+ * 
+ * @param {*} state requires at least the shape 
+ * { 
+ *  innings: list { top: { runs: number, hits: number }, bottom: { runs: number, hits: number } }, 
+ * }
+ */
 function isBottomHalf(state) {
     const numberOfInnings = state.innings.length
     return numberOfInnings > 0 && state.innings[numberOfInnings - 1].top && !state.innings[numberOfInnings - 1].bottom
@@ -11,12 +18,71 @@ function isTopHalf(state) {
     return numberOfInnings === 0 || !state.innings[numberOfInnings - 1].top || state.innings[numberOfInnings - 1].bottom
 }
 
+/**
+ * Returns true if
+ * - 8+ 1/2 innings have been played and the home team is winning
+ * - 9+ innings have been completed (top and bottom) and a team is winning
+ * 
+ * If the home team is ever winning after 9 innings, they win
+ * 
+ * @param {*} state requires at least the shape 
+ * { 
+ *  innings: list { top: { runs: number, hits: number }, bottom: { runs: number, hits: number } }, 
+ *  currentInning: { runs: number, hits: number }
+ * }
+ */
+function isEndState(state) {
+    return (
+        (state.innings.length >= 9 && getHomeTeamScore(state) > getVisitingTeamScore(state))
+        || (state.innings.length >= 9 && isTopHalf(state) && getVisitingTeamScore(state) > getHomeTeamScore(state))
+    )
+}
+
+/**
+ * Gets the score for the visiting team including the current inning (if it is the top half)
+ * 
+ * @param {*} state requires at least the shape 
+ * { 
+ *  innings: list { top: { runs: number, hits: number }, bottom: { runs: number, hits: number } }, 
+ *  currentInning: { runs: number, hits: number }
+ * }
+ */
+function getVisitingTeamScore(state) {
+    return state.innings
+        .filter(inning => inning.top)
+        .map(inning => inning.top.runs)
+        .reduce((x, y) => x + y, 0)
+        + ((isTopHalf(state) && state.currentInning) ? state.currentInning.runs : 0)
+
+}
+
+/**
+ * Gets the score for the home team including the current inning (if it is the bottom half)
+ * 
+ * @param {*} state requires at least the shape 
+ * { 
+ *  innings: list { top: { runs: number, hits: number }, bottom: { runs: number, hits: number } }, 
+ *  currentInning: { runs: number, hits: number }
+ * }
+ */
+function getHomeTeamScore(state) {
+    return state.innings
+        .filter(inning => inning.bottom)
+        .map(inning => inning.bottom.runs)
+        .reduce((x, y) => x + y, 0)
+        + ((isBottomHalf(state) && state.currentInning) ? state.currentInning.runs : 0)
+}
+
 function out(state, action) {
     const inningState = inningReducer(state.currentInning, action)
     if (inningState.outs === 3) {
         const inningSummary = {
             runs: inningState.runs,
             hits: inningState.hits
+        }
+
+        if (isEndState(state)) {
+
         }
 
         if (isTopHalf(state)) {
@@ -78,7 +144,12 @@ function reducer(state, action) {
                 currentInning: inningReducer(state.currentInning, action),
             })
     }
-    
 }
 
 export default reducer
+
+export {
+    getVisitingTeamScore,
+    getHomeTeamScore,
+    isEndState
+}
--- src/reducers/gameReducer.test.js
+++ src/reducers/gameReducer.test.js
@@ -1,4 +1,4 @@
-import reducer from './gameReducer'
+import reducer, { getHomeTeamScore, getVisitingTeamScore, isEndState } from './gameReducer'
 import { defaultInning } from './inningReducer'
 import actions from './actions'
 
@@ -83,8 +83,141 @@ test('bottom half of inning ending resets the state', () => {
     })
 })
 
-test('end state - end of the top of the ninth with the home team winning', () => {
+function createInning(visitingRuns, visitingHits, homeRuns, homeHits) {
+    return {
+        top: { runs: visitingRuns, hits: visitingHits },
+        bottom: { runs: homeRuns, hits: homeHits }
+    }
+}
 
+function createInnings(numberOfInnings, visitingRunsPerInning, visitingHitsPerInning, homeRunsPerInning, homeHitsPerInning) {
+    const innings = []
+    for (let i = 0; i < numberOfInnings; i = i + 1) {
+        innings.push(createInning(visitingRunsPerInning, visitingHitsPerInning, homeRunsPerInning, homeHitsPerInning))
+    }
+    return innings
+}
+
+const inning = { top: { runs: 0, hits: 1 }, bottom: { runs: 1, hits: 2 } }
+const eightInnings = [inning, inning, inning, inning, inning, inning, inning, inning]
+
+test('getVisitingTeamScore sums up the runs of all completed innings', () => {
+    const innings = [createInning(3, 3, 0, 0), createInning(2, 2, 0, 0), createInning(2, 2, 0, 0)]
+
+    const state = {
+        innings
+    }
+
+    expect(getVisitingTeamScore(state)).toEqual(7)
+})
+
+test('getVisitingTeamScore includes the current inning', () => {
+    const innings = [createInning(3, 3, 0, 0), createInning(2, 2, 0, 0), createInning(2, 2, 0, 0)]
+
+    const state = {
+        innings,
+        currentInning: {
+            runs: 1,
+            hits: 2
+        }
+    }
+
+    expect(getVisitingTeamScore(state)).toEqual(8)
+})
+
+test('getHomeTeamScore sums up the runs of all completed innings', () => {
+    const innings = [createInning(0, 0, 1, 1), createInning(0, 0, 2, 2), createInning(0, 0, 3, 3)]
+    
+    const state = {
+        innings
+    }
+
+    expect(getHomeTeamScore(state)).toEqual(6)
+})
+
+test('getHomeTeamScore includes the current inning', () => {
+    const innings = [
+        createInning(0, 0, 1, 1), 
+        createInning(0, 0, 2, 2), 
+        createInning(0, 0, 3, 3),
+        { top: { runs: 0, hits: 0 } }]
+
+    const state = {
+        innings,
+        currentInning: {
+            runs: 4,
+            hits: 2
+        }
+    }
+
+    expect(getHomeTeamScore(state)).toEqual(10)
+})
+
+test('isEndState is false in with no innings played', () => {
+    const state = {
+        innings: []
+    }
+
+    expect(isEndState(state)).toBe(false)
+})
+
+test('isEndState is false when teams are tied after 9 innings', () => {
+    const state = {
+        innings: createInnings(9, 0, 0, 0, 0)
+    }
+
+    expect(isEndState(state)).toBe(false)
+})
+
+test('isEndState is true when home team is winning after 8 1/2', () => {
+    const state = {
+        innings: eightInnings.concat({ top: { runs: 0, hits: 0 }})
+    }
+
+    expect(isEndState(state)).toBe(true)
+})
+
+test('isEndState is true when visiting team is winning at 9 innings played', () => {
+    const state = {
+        innings: createInnings(9, 1, 1, 0, 0)
+    }
+
+    expect(isEndState(state)).toBe(true)
+})
+
+// TODO visiting team winning after 10 innings and batting - true
+// TODO home team winning after 10 innings - true
+// TODO visiting team winning after 9 innings but home team has to bat - false
+
+xtest('end state - end of the top of the ninth with the home team winning', () => {
+    const inningState = Object.assign({}, defaultInning(visitingRoster[visitingIndex]), {
+        runs: 1,
+        outs: 2,
+        hits: 2
+    })
+
+    const state = {
+        homeIndex,
+        homeRoster,
+        visitingIndex,
+        visitingRoster,
+        currentInning: inningState,
+        innings: eightInnings
+    }
+
+    const newState = reducer(state, { type: actions.OUT })
+    
+    expect(newState).toEqual({
+        gameOver: true,
+        homeRoster,
+        visitingRoster,
+        innings: eightInnings.concat({
+            top: {
+                runs: 1,
+                hits: 2
+            }
+        })
+    })
 })
 
 test('end state - end of the bottom of the ninth', () => {
@@ -95,7 +228,7 @@ test('end state - extra innings', () => {
 
 })
 
-test('game', () => {
+xtest('game', () => {
     function log(message) {
         console.log(message)
     }    
